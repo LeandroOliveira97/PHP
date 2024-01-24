2c5ea58b2223ea1f19e4f5d3193667c913d7575e
LeandroPC <leandronotebook18@gmail.com>
2024-01-24 20:28:22 -0300

    Primeiro commit estudando PHP. Preparando ambiente

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+</head>
+<body>
+    <?php 
+    echo 'utilizando tag padrão'
+    ?> 
+
+    <?= 'Tag de impressão.'?>
+</body>
+</html>
